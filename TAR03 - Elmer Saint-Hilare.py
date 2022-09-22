fb333efa73e2de6186192c5c481b72f0e3800a84
Elmerluis0129 <es21-1354@unphu.edu.do>
2022-09-22 19:05:48 -0400

    Tarea de Nadia Disla

--- TAR03 - Elmer Saint-Hilare.py
+++ TAR03 - Elmer Saint-Hilare.py
@@ -15,23 +15,23 @@ print("*---------------*\n")#Decorando las opciones.
 
 #Declaro las variables a utilizar, tipo de instalacion y kilo waths consumidos.
 TipoInstalacion = str(input("Ingrese el tipo de instalación: ")) #Tipo de instalacion.
-kwhConsumido = int(input("Ingrese la cantidad de Kilo Waths consumidos: "))#Kilo Whath consumidos.
+kwhConsumido = int(input("Ingrese la cantidad de Kilo Watts consumidos: "))#Kilo Whatts consumidos.
 
 #Condiciones para saber que tipo de instalacion tienen los usuarios.
 if TipoInstalacion == "R":#Aqui para saber si su instalacion es tipo R.
-    if kwhConsumido == 1 or kwhConsumido <= 500:
+    if kwhConsumido == 1 or kwhConsumido <= 500:#Sentencia para saber si la cantidad de Kilo watts es menor a 500.
         print("\nEl precio a pagar es de: RD$550.00")#Precio a pagar dependiendo de si su consumo es menor a 500.
-    elif kwhConsumido > 500:
+    elif kwhConsumido > 500:#Sentencia para saber si la cantidad de Kilo watts es mayor a 500.
         print("\nEl precio a pagar es de: RD$850.00")#Precio a pagar dependiendo de si su consumo es mayor a 500.
 elif TipoInstalacion == "C":#Aqui para saber si su instalacion es tipo C.
-    if kwhConsumido == 1 or kwhConsumido <= 1000:
+    if kwhConsumido == 1 or kwhConsumido <= 1000:#Sentencia para saber si la cantidad de Kilo watts es menor a 1000.
         print("\nEl precio a pagar es de: RD$1300.00")#Precio a pagar dependiendo de si su consumo es menor a 1000.
-    elif kwhConsumido > 1000:
+    elif kwhConsumido > 1000:#Sentencia para saber si la cantidad de Kilo watts es mayor a 1000.
         print("\nEl precio a pagar es de: RD$2500.00")#Precio a pagar dependiendo de si su consumo es mayor a 1000.
 elif TipoInstalacion == "I":#Aqui para saber si su instalacion es tipo I.
-    if kwhConsumido == 1 or kwhConsumido <= 5000:
+    if kwhConsumido == 1 or kwhConsumido <= 5000:#Sentencia para saber si la cantidad de Kilo watts es menor a 5000.
         print("\nEl precio a pagar es de: RD$3800.00")#Precio a pagar dependiendo de si su consumo es menor a 5000.
-    elif kwhConsumido > 5000: 
+    elif kwhConsumido > 5000:#Sentencia para saber si la cantidad de Kilo watts es mayor a 5000.
         print("\nEl precio a pagar es de: RD$4200.00")#Precio a pagar dependiendo de si su consumo es mayor a 5000.
 else:
     print("\nLo siento, lo que haz introducido no es la letra que corresponde a uno de las opciones de tipo de instalacion. ") #En caso de que el usuario no marque correctamente la letra de su tipo de instalacion.
