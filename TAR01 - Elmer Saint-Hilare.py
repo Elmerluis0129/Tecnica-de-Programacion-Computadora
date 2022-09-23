7a95c9cc3305e234b939d515013aa622eab872f9
Elmerluis0129 <es21-1354@unphu.edu.do>
2022-09-23 09:31:50 -0400

    Aprendiendo GIT

--- TAR01 - Elmer Saint-Hilare.py
+++ TAR01 - Elmer Saint-Hilare.py
@@ -2,7 +2,6 @@
 print("\n*--------------------*")#Decorando el nombre.
 print("| Calculadora Básica |")
 print("*--------------------*\n")#Decorando el nombre.
-#Prueba
 
 #Operaciones para mostrarle en pantalla las que tiene para poder elegir.
 print("""\nOperaciones:\n
