a226afd84e4007b0f73065317aac260a76d442f1
Elmerluis0129 <es21-1354@unphu.edu.do>
2022-09-23 10:49:24 -0400

    Tarea de Nadia Disla

--- TAR01 - Elmer Saint-Hilare.py
+++ TAR01 - Elmer Saint-Hilare.py
@@ -21,9 +21,11 @@ modo = int(input("""¿Cómo quieres las respuesta/s? Responder con el número po
 
 #Si el usuario selecciona la opcion 2, hace la operacion que elija.
 if modo == 2:
-    print("\n*-------------------------------*")#Decorando los "Subtitulos".
-    print("| Modo: Selección de operación. |")
-    print("*-------------------------------*\n")#Decorando los "Subtitulos".
+    print("""\n
+*-------------------------------*
+| Modo: Selección de operación. |
+*-------------------------------*
+    \n""")#Decorando los "Subtitulos".
     #Eleccion de la operacion a realizar
     operacion = int(input("¿Qué operación desea realizar? / Responde con el número, por favor.\n> "))
 
