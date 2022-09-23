63fdec1cd3425897922acd29e54ae27f8dbe5002
Elmerluis0129 <es21-1354@unphu.edu.do>
2022-09-22 22:48:44 -0400

    Tarea de Nadia Disla

--- TAR04 - Elmer Saint-Hilare.py
+++ TAR04 - Elmer Saint-Hilare.py
@@ -1,33 +1,44 @@
 #Escriba un programa que permita crear una lista de palabras y que, a continuación, pida una palabra y diga cuántas veces aparece esa palabra en la lista.
 #Elmer Saint-Hilare 21/1354
 
-Lista = []
-x = 1
+#Declaro mis variables a utilizar.
+Lista = []#Aquí se almacenan todos los elementos que el usuario más adelante va a proporcionar.
+x = 1 #Aquí es para poder cambiar el mandato en el bucle for.
 
-print("*--------------------------------------------*")
+print("*--------------------------------------------*")#Decorando la intrucción.
 print("|Ingresar las 5 palabras para crear la lista.|")
-print("*--------------------------------------------*\n")
+print("*--------------------------------------------*\n")#Decorando la intrucción.
 
+#Bucle for para hacer las iteraciones necesarias para crear la lista, en este caso 5 veces.
 for n in range(5):
-    if x == 1:
+    #Aquí para saber qué mandato le va a dar al usuario dependiendo de qué valor tenga x en dicho momento.
+    if x == 1: #Aquí para la primera iteración tendrá un valor de 1 la x, por lo que le pondrá el 1er mandato.
         Lista.append(input("Ingresar la primera palabra para la lista: "))
-    elif x == 2:
+    elif x == 2: #Aquí para la segunda iteración tendrá un valor de 2 la x, por lo que le pondrá el 2do mandato.
         Lista.append(input("Ingresar la segunda palabra para la lista: "))
-    elif x == 3:
+    elif x == 3: #Aquí para la tercera iteración tendrá un valor de 3 la x, por lo que le pondrá el 2do mandato.
         Lista.append(input("Ingresar la tercera palabra para la lista: "))
-    elif x == 4:
+    elif x == 4: #Aquí para la cuarta iteración tendrá un valor de 4 la x, por lo que le pondrá el 2do mandato.
         Lista.append(input("Ingresar la cuarta palabra para la lista: "))
-    elif x == 5:
+    elif x == 5: #Aquí para la quinta iteración tendrá un valor de 5 la x, por lo que le pondrá el 2do mandato.
         Lista.append(input("Ingresar la quinta palabra para la lista: "))
-        break
-    x = x+1
+        break #Esto sirve para cuanddo vaya por la 5ta iteración y se cumpla la condición el bucle for se rompa, terminando por completo con las iteraciones.
+    x = x+1 #Este es la magia del bucle, aquí la variable x irá aumentando su valor de 1 en 1, por lo que es lo que ayuda a que cada condición se vaya cumpliendo según aumente el valor de x.
     
-buscarPalabra = input("Escriba la palabra que desea buscar: ")
-VecesRepetida = Lista.count(buscarPalabra)
+#Aquí creo las variables que me van a contener tanto la entrada por teclado del usuario, como las veces que aparece la palabra.
+buscarPalabra = input("\nEscriba la palabra que desea buscar: ") #Aqui para saber que palabra desea buscar en la lista.
+VecesPalabraAparece = Lista.count(buscarPalabra) #Para saber si la palabra aparece o no y cuantas veces en caso de sí.
 
-if VecesRepetida > 1:
-    print("Las veces que la palabra: ","'",buscarPalabra,"'", "se repite es: ",VecesRepetida, "veces.")
-elif VecesRepetida == 1:
-    print("Las veces que la palabra: ","'",buscarPalabra,"'", "se repite es: ",VecesRepetida, "vez.")
-else:
-    print("Las veces que la palabra: ","'",buscarPalabra,"'", "se repite es: ",VecesRepetida, "veces.") #TODO Recuerda agregarle que si una palabra no se encuentra en la lista, se lo notifique al usuario.
+#Aquí uso estas condiciones para saber qué cantidad de veces aparece la palabra, y poder varíar la respuesta, tipo: una vez o tantas veces. o no aparece.
+if VecesPalabraAparece > 1:
+    print("\nLa palabra: ","'",buscarPalabra,"'", "aparece",VecesPalabraAparece, "veces.") #Aquí si aparece más de 1 vez, usará "Veces".
+elif VecesPalabraAparece == 1:
+    print("\nLa palabra: ","'",buscarPalabra,"'", "aparece",VecesPalabraAparece, "vez.") #Aquí si aparece 1 sola vez, usará "Vez".
+elif not buscarPalabra in Lista:
+    print("\nLa palabra: ","'",buscarPalabra,"'", "no aparece en la lista que haz creado anteriormente.")  #Aquí si no aparece la palabra se lo especifica.
+
+#Agradecimiento por usar el programa.
+print("\n*----------------------------------------------------------------------------*")#Decorando el agradecimiento.
+print("| ¡Muchas gracias por utilizar mi programa! / By: Elmer Saint-Hilare 21-1354 |")
+print("*----------------------------------------------------------------------------*\n")#Decorando el agradecimiento.
+#FIN
