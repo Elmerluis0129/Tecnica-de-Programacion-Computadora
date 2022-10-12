268d4e71fafb14d7b4683e17fda6a5a263186928
Elmerluis0129 <es21-1354@unphu.edu.do>
2022-10-12 11:48:41 -0400

    Tarea de Nadia Disla

--- TAR09 - Elmer Saint-Hilare.py
+++ TAR09 - Elmer Saint-Hilare.py
@@ -3,4 +3,81 @@ Escriba un programa que, mediante una función, dada una cadena de caracteres (s
 Ejemplo:
 Ingrese una cadena de caracteres: aaabbbcdddefefef
 El primer caracter que no se repite es: c
+Elmer Saint-Hilare 21-1354
 """
+
+#============ Nombre programa ===========#
+"""
+Aquí es para imprimir el nombre del programa.
+"""
+from itertools import count
+
+
+print("""
+*------------------------*  
+|Primero que no se repite|
+*------------------------*
+\n""")
+#============ Fin Nombre programa ===========#
+
+#========================== Función ==========================#
+"""
+Aquí defino la función, la cual llamo luego para que me ejecute lo que tiene dentro.
+
+Dentro de la función tengo variables para ir midiendo la longitud de la cadena de caracteres.
+También tengo una condicional if para controlar los espacios, para que no ingrese espacios los usuarios.
+Luego tengo un while donde me ejecute ese código dentro mientras la longitud 2 sea diferente de la longitud 1 menos 1.
+Dentro del while tengo que cada vez que se iteree, me cambie el valor de la longitud 1 si es necesario, luego al primer caracter lo reemplazo con "", luego cambio longitud 2 si es necesario.
+Dentro del while también tengo otra condicional que es la que decide si imprimirme el primer carácter que no se repite, o si me dice que todos se repiten y termina el while.
+
+"""
+lista = []
+def primer_Caracter_no_se_repite(pcnsr):
+    cadenaCaracteresLongitud = len(pcnsr) 
+    cadenaCaracteresLongitud2 = len(pcnsr) 
+    
+    if " " in pcnsr:
+        print("\nFavor no introducir espacios.\nVuelva a intentarlo.\n")
+        primer_Caracter_no_se_repite(input("Ingrese la cadena de caracteres: \n>"))
+    else:
+        while True: 
+            cadenaCaracteresLongitud = len(pcnsr) 
+            print(lista)
+            primerCaracterRepetido = pcnsr[0]
+            pcnsr = pcnsr.replace(pcnsr[0], "")
+            
+            #print(s)
+            cadenaCaracteresLongitud2 = len(pcnsr)
+            #print(cadenaCaracteresLongitud2) 
+            
+            if cadenaCaracteresLongitud2 == cadenaCaracteresLongitud-1: 
+                print ("\nEl primer carácter que no se repite es: ", primerCaracterRepetido, "\n")
+                break
+            elif cadenaCaracteresLongitud != cadenaCaracteresLongitud-1:
+                serepite = lista.count(pcnsr)
+                print("\nSe repite: ",primerCaracterRepetido, serepite, "veces.")
+            
+                
+            
+#========================== Fin Función ==========================#
+
+#========================== Llamada Función ==========================# 
+"""
+Aquí lo uso para llamar la función.
+También pongo que la entrada de la función sea una entrada de teclado, para que la lista de caracteres la introduzca el usuario.
+"""       
+primer_Caracter_no_se_repite(input("Ingrese la cadena de caracteres: \n>"))
+#========================== Fin Llamada Función ==========================#    
+
+#========================== Agradecimiento por usar el programa =========================#  
+
+"""
+Aquí imprimo por pantalla el agradecimiento y por quién fue creado, en este caso por mi.
+"""
+
+print("""
+*----------------------------------------------------------------------------*
+| ¡Muchas gracias por utilizar mi programa! / By: Elmer Saint-Hilare 21-1354 |
+*----------------------------------------------------------------------------*
+""")
+#========================== Fin Agradecimiento por usar el programa =========================#
