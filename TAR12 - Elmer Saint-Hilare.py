8e1400bf3532f3406d6e20153fef4d0bfb367e1c
Elmerluis0129 <es21-1354@unphu.edu.do>
2022-10-21 10:32:41 -0400

    Tarea de Nadia Disla

--- TAR12 - Elmer Saint-Hilare.py
+++ TAR12 - Elmer Saint-Hilare.py
@@ -18,22 +18,38 @@ Si se coloca un destino que no se encuentra en los almacenados, el programa debe
 
 Elmer Saint-Hilare 21-1354.
 """
-
+# ============== Lista con los viajeros =============== #
+"""
+Aquí almaceno la lista de tuplas con la informacion de los usuarios.
+"""
 ListaTuplasViajeros = [
-    ('Juan', 30, 'Madrid'), 
+    ('Juan'    , 30, 'Madrid'), 
     ('Fernanda', 42, 'Portugal'), 
-    ('Raúl', 28, 'Brazil'), 
-    ('Julio', 32, 'Venezuela'), 
-    ('Mateo', 26, 'Argentina'), 
-    ('María', 32, 'Portugal'), 
-    ('José', 29, 'Madrid'), 
-    ('Marcos', 29, 'Venezuela'), 
-    ('Juana', 41, 'Venezuela'), 
-    ('Paulina', 35, 'Madrid')
+    ('Raúl'    , 28, 'Brazil'), 
+    ('Julio'   , 32, 'Venezuela'), 
+    ('Mateo'   , 26, 'Argentina'), 
+    ('María'   , 32, 'Portugal'), 
+    ('José'    , 29, 'Madrid'), 
+    ('Marcos'  , 29, 'Venezuela'), 
+    ('Juana'   , 41, 'Venezuela'), 
+    ('Paulina' , 35, 'Madrid')
 ]
+# ============ Fin Lista con los viajeros ============= #
+
+# ===== Listas Generales ===== #
+"""
+En estas listas procedo mas adelante a convertirlas segun lo necesite.
+Aquí guardo tanto los destinos como la informacion de cada pasajero.
+"""
 Diccionario = []
 ListaDestinos = []
+# === Fin Listas Generales === #
 
+# ==== For - Creando Diccionario / Destinos ==== #
+"""
+Aquí voy pasando la informacion de la lista de tuplas a un diccionario, indicando el nombre, edad y destino.
+Y aquí también agrego de la lista de tuplas solamente los destinos en la lista creada anteriormente.
+"""
 for clave in ListaTuplasViajeros:
     Diccionario.append(
         {
@@ -45,10 +61,21 @@ for clave in ListaTuplasViajeros:
     ListaDestinos.append(
         clave[2]
     )
+# == Fin For - Creando Diccionario / Destinos == #
     
+# =================== Funciones ======================= #
+"""
+Aquí con las funciones en la primera lo que hago es verificar si el destino ingresado por teclado es igual a uno de la lista ya mostrada.
+SI lo es entonces me reccorre las tuplas una vez mas y me verifica si la clave posicion 2 (Destino) es igual o no al destino seleccionado por el usuario
+Si lo es entonces me crea un diccionario con esa informacion, tanto asi que solo me crea un diccionario con los usuarios que coinciden con el destino buscado.
 
+Si el destino ingresado no esta en la lista, procede a mostrarle un mesanje avisandole que dicho destino no hay pasajero que vayan para el mismo.
+
+En la segunda función me encargo de la lista anterior de destinos que tenia, me ignora todas las repetidas ya que la paso de lista a set.
+Luego me imprime esos destinos.
+"""
 def BuscarPasajeroDestino(n):
-    m = 1
+    m = 0
     if n in ListaDestinos:
         print("\nPasajeros que van para {}:\n".format(n))
         for clave in ListaTuplasViajeros:
@@ -57,9 +84,13 @@ def BuscarPasajeroDestino(n):
                         "Nombre": clave[0],
                         "Edad": clave[1],
                         "Destino": clave[2]
-                     }    
+                     }   
+                m = m + 1     
                 print("|{}.|".format(m), Diccionario2)
-                m = m + 1      
+        if m == 1:
+            print("\nSe encontró: {} pasajero.".format(m)) 
+        else:
+            print("\nSe encontraron: {} pasajeros.".format(m))  
     else:
         print("\n¡Lo siento! No hay pasajeros con destino a '{}'.".format(n))
     
@@ -68,10 +99,17 @@ def destino(x=0):
     Destinos = set(ListaDestinos)
     print("\nDestinos: {}".format(Destinos))
     print("\nIngrese el destino para saber quien va a viajar para el mismo.")
-    BuscarPasajeroDestino(input("\nDestino: \n> "))
-    
 
-destino()
+
+# ================= Fin Funciones ===================== #
+
+# ============ Llamada Funciones ============ #
+"""
+Aquí es donde llamo mis funciones con el orden correspondiente.
+"""
+destino() 
+BuscarPasajeroDestino(input("\nDestino: \n> "))
+# ========== Fin Llamada Funciones ========== #
 
 #========================== Agradecimiento por usar el programa =========================#  
 
