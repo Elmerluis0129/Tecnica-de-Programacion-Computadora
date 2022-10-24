354ed141866bd4ce270fa73f17dd94c912cc75bb
Elmerluis0129 <es21-1354@unphu.edu.do>
2022-10-24 19:27:59 -0400

    Tarea De Nadia Disla

---
+++ TAR13 - Elmer Saint-Hilare.py
@@ -0,0 +1,22 @@
+"""
+Escriba un programa que mediante módulos tenga el siguiente menú de opciones, y realice lo que se indica para cada una de ellas:
+
+Diccionario de caracteres. Solicitar una cadena de caracteres y devolver un diccionario con la cantidad de apariciones de cada caracter (sin repetir) en la cadena. Si la cadena tiene espacios, el diccionario debe tener la clave 'espacio'.
+Formato de fechas. Solicitar una fecha por teclado (día, mes y año), y retornarla en el siguiente formato de ejemplo: Sábado 29 de octubre del 2022.
+
+Elmer Saint-Hilare 21-1354
+
+"""
+
+#========================== Agradecimiento por usar el programa =========================#  
+
+"""
+Aquí imprimo por pantalla el agradecimiento y por quién fue creado, en este caso por mi.
+"""
+
+print("""
+*----------------------------------------------------------------------------*
+| ¡Muchas gracias por utilizar mi programa! / By: Elmer Saint-Hilare 21-1354 |
+*----------------------------------------------------------------------------*
+""")
+#========================== Fin Agradecimiento por usar el programa =========================#
