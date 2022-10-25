dc507b562f8e9e3617780163fe739b23b847ec70
Elmerluis0129 <es21-1354@unphu.edu.do>
2022-10-25 19:10:03 -0400

    Tarea de Nadia Disla

--- PP02 - Elmer Saint-Hilare.py
+++ PP02 - Elmer Saint-Hilare.py
@@ -7,6 +7,8 @@ Formato de fechas. Solicitar una fecha por teclado (día, mes y año), y retorna
 Elmer Saint-Hilare 21-1354
 
 """
+
+
 x = "1"
 print("""
 *--------------------------------------*
@@ -37,56 +39,80 @@ print("""
 |12.-| Los viajeros.                   |
 *--------------------------------------*
 """)
+
+print("Si quieres salir solo tienes que escribir: Fin")
 while True:
-    if x == "1":
+    if x == 3:
+        break
+    elif x == "1":
         eleccion = input("¿Qué programa desea ejecutar? \n> ")
     if eleccion == "1":
         import Módulos.TAR01_Elmer_Saint_Hilare
+        
         x = "1"
+        print("Recuerda que si quieres salir solo tienes que escribir: Fin")
     elif eleccion == "2":
         import Módulos.TAR02_Elmer_Saint_Hilare
+        print("Recuerda que si quieres salir solo tienes que escribir: Fin")
         x = "1"
     elif eleccion == "3":
         import Módulos.TAR03_Elmer_Saint_Hilare
         x = "1"
+        print("Recuerda que si quieres salir solo tienes que escribir: Fin")
     elif eleccion == "4":
         import Módulos.TAR04_Elmer_Saint_Hilare
         x = "1"
+        print("Recuerda que si quieres salir solo tienes que escribir: Fin")
     elif eleccion == "5":
         import Módulos.TAR05_Elmer_Saint_Hilare
         x = "1"
+        print("Recuerda que si quieres salir solo tienes que escribir: Fin")
     elif eleccion == "6":
         import Módulos.TAR06_Elmer_Saint_Hilare
         x = "1"
+        print("Recuerda que si quieres salir solo tienes que escribir: Fin")
     elif eleccion == "7":
         import Módulos.TAR07_Elmer_Saint_Hilare
         x = "1"
+        print("Recuerda que si quieres salir solo tienes que escribir: Fin")
     elif eleccion == "8":
         import Módulos.TAR08_Elmer_Saint_Hilare
         x = "1"
+        print("Recuerda que si quieres salir solo tienes que escribir: Fin")
     elif eleccion == "9":
         import Módulos.TAR09_Elmer_Saint_Hilare
         x = "1"
+        print("Recuerda que si quieres salir solo tienes que escribir: Fin")
     elif eleccion == "10":
         import Módulos.TAR10_Elmer_Saint_Hilare
         x = "1"
+        print("Recuerda que si quieres salir solo tienes que escribir: Fin")
     elif eleccion == "11":
         import Módulos.TAR11_Elmer_Saint_Hilare
         x = "1"
+        print("Recuerda que si quieres salir solo tienes que escribir: Fin")
     elif eleccion == "12":
         import Módulos.TAR12_Elmer_Saint_Hilare
         x = "1"
+        print("Recuerda que si quieres salir solo tienes que escribir: Fin")
+        
+    elif eleccion == "Fin":
+        print("\nCerrando el programa general...")
+        break
     elif eleccion != "12":
         while True:
+            
             print("Lo siento, lo que haz introducido no va acorde con los números del menú de opciones.")
             print("Intenta de nuevo.\n")
             eleccion = input("¿Qué programa desea ejecutar? \n> ")
             if eleccion == "1"  or eleccion == "2" or eleccion == "3" or eleccion == "4" or eleccion == "5" or eleccion == "6" or eleccion == "7" or eleccion == "8" or eleccion == "9" or eleccion == "10" or eleccion == "11" or eleccion == "12":
                 x = "2"
                 break
+            elif eleccion == "Fin":
+                print("\nCerrando el programa general...")
+                x = 3
+                break     
         continue
-    else:
-        break
             
 #Módulos.TAR01_Elmer_Saint_Hilare
 
