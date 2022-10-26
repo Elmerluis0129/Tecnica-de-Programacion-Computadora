c6f813405c63a76372e849942537124d4102dd86
Elmerluis0129 <es21-1354@unphu.edu.do>
2022-10-26 12:09:25 -0400

    Tarea de Nadia Disla

---
+++ TAR13/DiccionarioCaracteres.py
@@ -0,0 +1,45 @@
+"""
+Diccionario de caracteres. Solicitar una cadena de caracteres y devolver un diccionario con la cantidad de apariciones de cada caracter (sin repetir) en la cadena. Si la cadena tiene espacios, el diccionario debe tener la clave 'espacio'.
+Elmer Saint-Hilare 21-1354
+"""
+
+
+
+
+ListaCaracteres1 = []
+ListaCaracteres2 = []
+Diccionario1 = {}
+
+cadenaCaracteres = input("Ingrese una cadena de caracteres: \n> ")
+
+
+
+
+for x in cadenaCaracteres:
+    ListaCaracteres1.append(x)
+        
+n = 0
+    
+for x in ListaCaracteres1:
+    VecesRepite = ListaCaracteres1.count(x)
+
+    if x == ' ':
+        Diccionario1.update(
+            {
+                'espacio':VecesRepite
+            }
+            
+        )
+      
+    else:
+        
+        Diccionario1.update(
+            {
+                x:"{}".format(VecesRepite)
+            }
+        
+        )
+
+
+
+print(Diccionario1)
--- TAR13/FormatoFecha.py
+++ TAR13/FormatoFecha.py
@@ -1,4 +1,7 @@
-# Elmer Saint-Hilare 21-1354
+"""
+Formato de fechas. Solicitar una fecha por teclado (día, mes y año), y retornarla en el siguiente formato de ejemplo: Sábado 29 de octubre del 2022.
+Elmer Saint-Hilare 21-1354
+""" 
 
 from datetime import date
 
