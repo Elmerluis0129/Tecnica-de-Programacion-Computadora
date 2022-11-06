d247a1bc568d2b4738e744742af6ac006d2a0443
Elmerluis0129 <es21-1354@unphu.edu.do>
2022-11-05 23:02:54 -0400

    Cursos Programación - Elmer

---
+++ Cursos/Cursos de programación/Cursos-Clases Python 3.9/Cursos en proceso/YouTube- pildorasinformaticas/5- Uso de modulos/Funciones.py
@@ -0,0 +1,10 @@
+from Paquetes.Funciones.Matematicas import * #Aqui utilizamos el modulo de mi funciones de matematicas
+
+op1 = int(input("Introduce el número uno: "))
+op2 = int(input("Introduce el número dos: "))
+print("****************************\n")
+
+sumar(op1, op2)
+restar(op1, op2)
+dividir(op1, op2)
+multiplicar(op1, op2)
---
+++ Cursos/Cursos de programación/Cursos-Clases Python 3.9/Cursos en proceso/YouTube- pildorasinformaticas/5- Uso de modulos/Paquetes/Funciones/Matematicas.py
@@ -0,0 +1,11 @@
+def sumar(op1, op2):
+	print(f"El resultado de la suma {op1} + {op2} es:\n> ", op1 + op2)
+
+def restar(op1, op2):
+	print(f"El resultado de la resta {op1} - {op2} es:\n> ", op1 - op2)
+
+def dividir(op1, op2):
+	print(f"El resultado de la divición {op1} / {op2} es:\n> ", op1 / op2)
+
+def multiplicar(op1, op2):
+	print(f"El resultado de la multiplicación {op1} * {op2} es:\n> ", op1 * op2)
